338d34496adacb9a0982314f1ef317450f45d324
Weilun Yao <yaoweilun1998@hotmail.com>
2018-10-27 22:17:49 -0700

    new file created

---
+++ GoogleMap.js
@@ -0,0 +1,7 @@
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(32.8801,-117.2340),
+    zoom:15,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
