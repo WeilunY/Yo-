0f8a69ff5a6c9f05b4fdac85a312deb7467c0877
Weilun Yao <yaoweilun1998@hotmail.com>
2018-10-27 22:59:10 -0700

    geolocation test

--- JS/GoogleMap.js
+++ JS/GoogleMap.js
@@ -1,7 +1,44 @@
-function myMap() {
+/**function myMap() {
 var mapProp= {
     center:new google.maps.LatLng(32.8801,-117.2340),
     zoom:15,
 };
 var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}*/
+
+var map, infoWindow;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 15
+  });
+  infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('YOU ARE HERE :)');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
 }
