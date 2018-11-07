e7018d299b02c63271412ecc190f496113ada3d7
Weilun Yao <yaoweilun1998@hotmail.com>
2018-11-06 22:42:51 -0800

    php login sign up

--- phptest/register.php
+++ phptest/register.php
@@ -1,13 +1,28 @@
 <?php
   require '../config/config.php';
   require '../includes/form_handler/register_handler.php';
+  require '../includes/form_handler/login_handler.php';
 ?>
+
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title></title>
   </head>
   <body>
+    <form = "register.php" method = "POST">
+      <input type="email" name="log_email" placeholder="Email Address" value="<?php
+      if(isset($_SESSION['log_email'])){
+          echo $_SESSION['log_email'];
+        }
+      ?>" required>
+      <br>
+      <input type="password" name="log_password" placeholder="Password">
+      <br>
+      <input type="submit" name="login_button" value="Login">
+      <?php if(in_array("Email or password was incorrect<br>", $error_array)) echo "Email or password was incorrect<br>"; ?>
+    </form>
+
     <form action="register.php" method="POST">
       <input type="text" name="reg_fname" placeholder="First Name" value="<?php
       if(isset($_SESSION['reg_fname'])){
