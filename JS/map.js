8d242d7527fc344ed517c655f8b84f7b93495bf4
Weilun Yao <yaoweilun1998@hotmail.com>
2018-11-14 19:57:50 -0800

    working talks near you

---
+++ JS/map.js
@@ -0,0 +1,19 @@
+/* Open when someone clicks on the span element */
+function openFeeds() {
+    document.getElementById("feeds").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeFeeds() {
+    document.getElementById("feeds").style.width = "0%";
+}
+
+/* Open when someone clicks on the span element */
+function openChat() {
+    document.getElementById("chat").style.width = "70%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeChat() {
+    document.getElementById("chat").style.width = "0%";
+}
