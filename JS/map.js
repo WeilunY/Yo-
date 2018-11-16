43b296a761953f9fd952d249e148e2681104317f
Weilun Yao <yaoweilun1998@hotmail.com>
2018-11-15 20:49:29 -0800

    post all 3 categories

--- JS/map.js
+++ JS/map.js
@@ -19,11 +19,27 @@ function closeChat() {
 }
 
 function openPost() {
-  document.getElementById("post").style.height = "90%";
+  document.getElementById("generalPost").style.height = "90%";
 }
 
 function closePost() {
-  document.getElementById("post").style.height = "0%";
+  document.getElementById("generalPost").style.height = "0%";
+}
+
+function openPost1() {
+  document.getElementById("academicPost").style.height = "90%";
+}
+
+function closePost1() {
+  document.getElementById("academicPost").style.height = "0%";
+}
+
+function openPost2() {
+  document.getElementById("eventPost").style.height = "90%";
+}
+
+function closePost2() {
+  document.getElementById("eventPost").style.height = "0%";
 }
 
 
@@ -52,9 +68,8 @@ var numPost = 6;
 // Create a genral post
 $('#general-post').click(function(){
   var title = $('#general-title');
-  var subject = $('#general-subject');
   var content = $('#general-content');
-  if (! title.val() || !subject.val() || !content.val()) {
+  if (! title.val() || !content.val()) {
     alert("Please fill out the form");
   } else {
     $("#midpost").prepend("<div class='filter general show'>\
@@ -64,14 +79,13 @@ $('#general-post').click(function(){
     <h2>" + title.val() + "</h2>\
     </div>\
     <h7 style='color: grey;'>now</h7>\
-    <h4> Subject: "+ subject.val() + "</h4>\
     <p>" + content.val() + "</p>\
     <div align='right' class='bgroup'>\
     <button class='btn btn-outline-success'> <i class='fa fa-thumbs-up'></i> Like </button>\
+    <button class='btn btn-outline-primary' onclick='typeComment( " + numPost + " )'> <i class='fa fa-comment'></i> Comment </button>\
     <div id='type" + numPost  + "' style='display: none;'>\
     <textarea class='form-control comment' id='content' rows='4' placeholder='Add your comment...''></textarea>\
     </div>\
-    <button class='btn btn-outline-primary' onclick='typeComment( " + numPost + " )'> <i class='fa fa-comment'></i> Comment </button>\
     </div> </div> </div>");
 
     numPost += 1;
@@ -82,5 +96,78 @@ $('#general-post').click(function(){
     subject.val("");
     content.val("");
   }
+});
 
+// Create academic
+$('#academic-post').click(function(){
+  var title = $('#academic-title');
+  var subject = $('#academic-subject')
+  var content = $('#academic-content');
+  if (! title.val() || !content.val() || !subject.val()) {
+    alert("Please fill out the form");
+  } else {
+    $("#midpost").prepend("<div class='filter academic show'>\
+    <div class='card' id='card-t'>\
+    <div id='container'>\
+    <img src='../Asset/academic.svg' width='35' height='35' align='left' style='margin-right: 20px;'/>\
+    <h2>" + title.val() + "</h2>\
+    </div>\
+    <h7 style='color: grey;'>now</h7>\
+    <h4> Subject: " + subject.val() + "</h4>\
+    <p>" + content.val() + "</p>\
+    <div align='right' class='bgroup'>\
+    <button class='btn btn-outline-success'> <i class='fa fa-thumbs-up'></i> Like </button>\
+    <button class='btn btn-outline-primary' onclick='typeComment( " + numPost + " )'> <i class='fa fa-comment'></i> Comment </button>\
+    <div id='type" + numPost  + "' style='display: none;'>\
+    <textarea class='form-control comment' id='content' rows='4' placeholder='Add your comment...''></textarea>\
+    </div>\
+    </div> </div> </div>");
+
+    numPost += 1;
+
+    closePost1();
+
+    title.val("");
+    subject.val("");
+    content.val("");
+  }
+});
+
+// Create an event post
+$('#event-post').click(function(){
+  var title = $('#event-title');
+  var location = $('#event-location');
+  var time = $('#event-date');
+  var description = $('#event-description');
+
+  if(!title.val() || !location.val() || !time.val() || !description.val()){
+    alert("Please fill out all the forms 2");
+  } else {
+    $("#midpost").prepend("<div class='filter event show'>\
+    <div class='card' id='card-t'>\
+    <div id='container'>\
+    <img src='../Asset/event.svg' width='35' height='35' align='left' style='margin-right: 20px;'/>\
+    <h2>" + title.val() + "</h2>\
+    </div>\
+    <h7 style='color: grey;'>now</h7>\
+    <h4> Location @  "+ location.val() + "</h4>\
+    <h5> Date: "+ time.val() + "</h5>\
+    <p>" + description.val() + "</p>\
+    <div align='right' class='bgroup'>\
+    <button class='btn btn-outline-success'> <i class='fa fa-thumbs-up'></i> Like </button>\
+    <button class='btn btn-outline-primary' onclick='typeComment( " + numPost + " )'> <i class='fa fa-comment'></i> Comment </button>\
+    <div id='type" + numPost  + "' style='display: none;'>\
+    <textarea class='form-control comment' id='content' rows='4' placeholder='Add your comment...''></textarea>\
+    </div>\
+    </div> </div> </div>");
+
+    numPost += 1;
+
+    closePost2();
+
+    title.val("");
+    location.val("");
+    time.val("");
+    content.val("");
+  }
 });
