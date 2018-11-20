c39c973c39a2dbc809fb43100aef2d5cc422e36b
Weilun Yao <yaoweilun1998@hotmail.com>
2018-11-20 12:53:05 -0800

    post emoji updated

--- JS/GoogleMap.js
+++ JS/GoogleMap.js
@@ -34,7 +34,7 @@ function getRandomEmojiList(num){
     // emoji img
     single.push('1f' + emoji + '.png');
     // title
-    single.push('TITILE');
+    single.push('TITILE Lorem ipsum dolor sit amet, consectetur adipiscing elit....');
     // content
     single.push(textPlaceholder);
     // num
@@ -254,9 +254,9 @@ function initMap() {
           info.setContent('<h3 style="font-size: 21px;">' + emojis[i][0]+'  '+ emojis[i][4] + '</h3>' +
                     '<h4 style="font-size: 14px;"> by John Doe </h4>' +
                     '<h4 style="color: grey; font-size: 12px;">' + getRandomEmoji(2,80) +' mins ago </h4>' +
-                    '<p style="font-size: 15px;">' + emojis[i][5] + '</p>' +
+                    //'<p style="font-size: 15px;">' + emojis[i][5] + '</p>' +
                   //'<button class="btn btn-outline-secondary" onclick="openFeeds()"><i class="fas fa-rss-square"></i> View in Feed </button>' +
-                '<button class = "btn btn-success" style="margin-left: 20px;" onclick="openChat()"> <i class="fas fa-comments"></i> Chat </button>');
+                '<button class = "btn btn-success" onclick="openChat()"> <i class="fas fa-comments"></i> Chat </button>');
           info.setOptions({maxWidth: 320});
           info.open(map, marker);
         }
@@ -323,19 +323,26 @@ function CenterControl(controlDiv, map) {
         $(emojiList).on('change', function(){
           var num = $(this).val();
           emoji =  num;
-          if(confirm("Post &#x1F" + emoji +"?" )){
-            postEmoji(map, emoji);
+          var title = prompt("What would you like to share?");
+
+          if(title != null){
+            postEmoji(map, emoji, title);
           }
-          
+
         });
 
         // Setup the click event listeners: post emoji
         controlUI.addEventListener('click', function() {
-          postEmoji(map, emoji);
+          var title = prompt("What would you like to share?");
+
+          if(title != null){
+            postEmoji(map, emoji, title);
+          }
+          postEmoji(map, emoji, title);
         });
       }
 
-function postEmoji(map, emoji){
+function postEmoji(map, emoji, title){
 
   infoWindow = new google.maps.InfoWindow;
 
@@ -361,7 +368,11 @@ function postEmoji(map, emoji){
 
       google.maps.event.addListener(marker, 'click', (function(marker) {
         return function() {
-          infoWindow.setContent("&#x1F" + emoji);
+          infoWindow.setContent('<h3 style="font-size: 21px;">' + "&#x1F" + emoji + ' ' +  title + '</h3>'+
+          '<h4 style="font-size: 14px;"> by You </h4>' +
+          '<h4 style="color: grey; font-size: 12px;"> Just Now </h4>'
+
+        );
           infoWindow.open(map, marker);
         }
       })(marker));
